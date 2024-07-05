6455d546d60e4a21eaa4592f3bc23997acdaa7fb
bangnokia <bangnokia@gmail.com>
2024-07-05 17:01:22 +0700

    wip

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import {defineConfig} from 'vitepress'
 export default defineConfig({
     title: "Portal docs",
     description: "Portal docs",
+    ignoreDeadLinks: true,
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
         nav: [
